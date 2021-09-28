3cd1e893315a8e1b2fe17c4b67e5c219d4257389
juhwan1014 <jmoon63@my.bcit.ca>
2021-09-27 22:24:21 -0700

    space

--- src/components/About.js
+++ src/components/About.js
@@ -14,7 +14,7 @@ class About extends Component {
             <Fade bottom>
             <img src={me} alt={"me"}></img>
                     <p> I graduated from Dankook University in South Korea and my major was mobile system engineering. Now, I'm completing 'Software Systems Developer Course(Web Programmer Option)' in BCIT.<br></br><br></br> I specialize in building and design applications specific to the business needs of my clients. I have done work in software development, mobile app creation, front-end/back-end web, database/server management, web design.</p>
-            </Fade>
+            </Fade>   
            
             </div>
           
