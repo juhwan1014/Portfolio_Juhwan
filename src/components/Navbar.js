fcad74c10b3ef100dd36fec702b85a9c2b20d67c
juhwan1014 <jmoon63@my.bcit.ca>
2021-09-29 01:15:18 -0700

    delete script

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,15 +9,7 @@ class Navbar extends Component {
     };
 
     componentDidMount() {
-        // $(document).ready(function(){
-        //     $(window).scroll(function(){
-        //         if(this.scrollY > 20){
-        //               $('.navbar').addClass("sticky");
-        //         } else {
-        //             $('.navbar').removeClass("sticky");
-        //         }
-        //     })
-        // });
+      
        
         $(window).scroll(function(){
            
