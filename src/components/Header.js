d4a6e6becb2c9775bae9c97e67f873bea29fd0ad
juhwan1014 <jmoon63@my.bcit.ca>
2021-09-30 20:47:07 -0700

    comment modify

--- src/components/Header.js
+++ src/components/Header.js
@@ -20,10 +20,10 @@ class Header extends Component {
             
            <div className='moon'> I'm "Juhwan Moon"</div><br></br>
            <div className='introduce'>
-            who is going to be a skillful developer for your company<br></br>
+            who is going to be a skillful developer<br></br>
              <span>Junior Full Stack Developer</span><br></br>
              </div>
-            
+                
                     <button className='contact_button'><a href={`mailto:$juhwan0718@gmail.com`} rel="noopener noreferrer" >Contact</a></button>
 
                 </p>
